ece77dd9375ff8e54db27b8afcee4a85c5c94878
DenisBit <67708768+DenisBit@users.noreply.github.com>
2023-02-08 11:54:30 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zi-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'zi-test';
+}
